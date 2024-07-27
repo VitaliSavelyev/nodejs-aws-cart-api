4674f709f4debf93ae04d1e1a02cd3b1ce03dd51
Vitali <vsavelyev@elinext.com>
2024-07-27 18:41:05 +0200

    task-8 added lambda for cart repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,30 @@ import { AppController } from './app.controller';
 import { CartModule } from './cart/cart.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cart } from './entity/Cart';
+import { CartItem } from './entity/CartItem';
+import * as dotenv from 'dotenv';
+import * as process from 'process';
+
+dotenv.config()
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DATABASE,
+      entities: [Cart, CartItem],
+      synchronize: true,
+      autoLoadEntities: true,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }),
     AuthModule,
     CartModule,
     OrderModule,
